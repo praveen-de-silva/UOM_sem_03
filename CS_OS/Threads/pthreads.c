c5193e3a0edae89cd352c6242332aea78411b2c8
Praveen De Silva <desilvabkp.23@uom.lk>
2025-08-09 15:39:35 +0530

    Update pthreads.c
    added .c file

--- CS_OS/Threads/pthreads.c
+++ CS_OS/Threads/pthreads.c
@@ -1,1 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int sum; // Shared global variable between threads
+void *runner(void *param); // Thread function prototype
+
+int main(int argc, char *argv[]) {
+    pthread_t tid; // Thread ID
+    pthread_attr_t attr; // Thread attributes (e.g., stack size)
+
+    // Initialize thread attributes with defaults
+    pthread_attr_init(&attr);
+
+    // Create a thread: runs `runner()` with argument `argv[1]`
+    pthread_create(&tid, &attr, runner, argv[1]);
+
+    // Wait for the thread to finish (join)
+    pthread_join(tid, NULL);
+
+    printf("sum = %d\n", sum);
+    return 0;
+}
+
+// Thread function: calculates sum of numbers up to `param`
+void *runner(void *param) {
+    int upper = atoi(param); // Convert string arg to integer
+    sum = 0;
+    for (int i = 1; i <= upper; i++) {
+        sum += i;
+    }
+    pthread_exit(0); // Terminate thread
+}
